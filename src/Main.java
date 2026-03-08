b00b1a2dd369eb57b2fdeda3d1aa33bb946409a0
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-03-08 15:35:34 -0500

    RELACION UML HERENCIA

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        Perro p = new Perro();
+
+        p.SONIDO();
+        p.LADRAR();
+
+    }
+}
+
+class Animal{
+
+    public void SONIDO(){
+        System.out.println("EL ANIMAL HACE UN SONIDO");
+    }
+
+}
+
+class Perro extends Animal{
+
+    public void LADRAR(){
+        System.out.println("GUAU GUAU");
+    }
+
+}
