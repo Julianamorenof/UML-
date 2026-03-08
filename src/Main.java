86153d4b0e5415fd18671666489f8553971c706e
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-03-08 15:39:17 -0500

    RELACION UML REALIZACION

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,28 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        Carro c = new Carro();
+
+        c.ACELERAR();
+
+    }
+}
+
+interface Vehiculo{
+
+    void ACELERAR();
+
+}
+
+class Carro implements Vehiculo{
+
+    public void ACELERAR(){
+
+        System.out.println("EL CARRO ESTA ACELERANDO");
+
+    }
+
+}
