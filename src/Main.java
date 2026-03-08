94d32cc30f9e14552465d160f14db3369a08cd4b
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-03-08 15:45:35 -0500

    RELACION UML DEPENDENCIA

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,38 @@
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        /*
+        RELACIÓN UML: DEPENDENCIA
+
+        La clase Profesor usa temporalmente
+        un objeto de tipo Libro dentro de un método.
+        */
+
         Profesor p = new Profesor();
         Libro l = new Libro();
 
-        p.ENSEÑAR(l);
+        p.enseñar(l);
 
     }
 }
 
 class Libro{
 
-    public void LEER(){
+    public void leer(){
+
         System.out.println("LEYENDO LIBRO");
+
     }
 
 }
 
 class Profesor{
 
-    public void ENSEÑAR(Libro libro){
+    // Dependencia: el profesor usa el libro como parámetro
+    public void enseñar(Libro libro){
 
-        libro.LEER();
+        libro.leer();
 
     }
 
