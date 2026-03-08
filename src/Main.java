1c431c60d2a271547938a5db651317138df3974d
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-03-08 15:37:55 -0500

    RELACION UML HERENCIA

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        Profesor p = new Profesor();
+        Libro l = new Libro();
+
+        p.ENSEÑAR(l);
+
+    }
+}
+
+class Libro{
+
+    public void LEER(){
+        System.out.println("LEYENDO LIBRO");
+    }
+
+}
+
+class Profesor{
+
+    public void ENSEÑAR(Libro libro){
+
+        libro.LEER();
+
+    }
+
+}
