563bef05126713492c4776e7ca90a38d95b5537f
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-03-08 15:33:31 -0500

    RELACION UML COMPOSICION

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,34 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        Carro c = new Carro();
+
+        c.ARRANCAR();
+
+    }
+}
+
+class Motor{
+
+    public void ENCENDER(){
+        System.out.println("EL MOTOR ESTA ENCENDIDO");
+    }
+
+}
+
+class Carro{
+
+    private Motor motor;
+
+    public Carro(){
+        motor = new Motor();
+    }
+
+    public void ARRANCAR(){
+        motor.ENCENDER();
+    }
+
+}
