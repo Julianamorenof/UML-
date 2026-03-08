c6ab85bc72cea1d26de83ddf03f24101bf67d6b8
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-03-08 15:45:06 -0500

    RELACION UML HERENCIA

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,40 @@
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        /*
+        RELACIÓN UML: HERENCIA
+
+        La clase Perro hereda de la clase Animal.
+        Esto significa que Perro obtiene los
+        atributos y métodos de Animal.
+        */
+
         Perro p = new Perro();
 
-        p.SONIDO();
-        p.LADRAR();
+        p.sonido();
+        p.ladrar();
 
     }
 }
 
 class Animal{
 
-    public void SONIDO(){
+    public void sonido(){
+
         System.out.println("EL ANIMAL HACE UN SONIDO");
+
     }
 
 }
 
+// Herencia: Perro extiende Animal
 class Perro extends Animal{
 
-    public void LADRAR(){
+    public void ladrar(){
+
         System.out.println("GUAU GUAU");
+
     }
 
 }
