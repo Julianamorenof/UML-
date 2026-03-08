cef9d1a24751f4f21ab37ded8da4c932d9bb6aa6
julianamoreno <jmorenof@ucundinamarca.edu.co>
2026-03-08 15:20:26 -0500

    primer commit

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,49 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("INGRESE EL NOMBRE DE LA UNIVERSIDAD: ");
+        String uni = teclado.next();
+
+        Universidad u = new Universidad(uni);
+
+        System.out.println("INGRESE EL NOMBRE DEL ESTUDIANTE:");
+        String nombre = teclado.next();
+
+        Estudiante e = new Estudiante(nombre, u);
+
+        e.mostrar();
+
+    }
+}
+
+class Universidad{
+
+    String nombre;
+
+    public Universidad(String nombre){
+        this.nombre = nombre;
+    }
+
+}
+
+class Estudiante{
+
+    String nombre;
+    Universidad universidad;
+
+    public Estudiante(String nombre, Universidad universidad){
+        this.nombre = nombre;
+        this.universidad = universidad;
+    }
+
+    public void mostrar(){
+        System.out.println(nombre + " estudia en la " + universidad.nombre);
+    }
+
+}
 
